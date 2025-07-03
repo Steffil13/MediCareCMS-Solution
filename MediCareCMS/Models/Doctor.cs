756a058a44fa18716c8ad4e41f12c377a56cd809
steffil13 <ashinsteffil13@gmail.com>
2025-07-03 22:22:13 +0530

    Updated Doctor model

--- MediCareCMS/Models/Doctor.cs
+++ MediCareCMS/Models/Doctor.cs
@@ -2,19 +2,19 @@
 {
     public class Doctor
     {
-      
-            public int DoctorId { get; set; }
-            public string DoctorName { get; set; }
-            public string DocEmpId { get; set; }
-            public string Phone { get; set; }
-            public int DeptId { get; set; }
-            public int UserId { get; set; }
-            public bool IsActive { get; set; }
 
-            public string Username { get; set; }
-            public string Password { get; set; }
-            public string Name { get; set; }
-            public string Contact { get; set; }
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public string DocEmpId { get; set; }
+        public string Phone { get; set; }
+        public int DeptId { get; set; }
+        public int UserId { get; set; }
+        public bool IsActive { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
     }
-    }
+}
 
