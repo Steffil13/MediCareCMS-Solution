eaae5997997bc1f3b3fbeaf50a1bcce4a5bbb376
veena-nath <jrveena01@gmail.com>
2025-07-09 13:24:28 -0700

    prescription model

--- MediCareCMS/Models/Prescription.cs
+++ MediCareCMS/Models/Prescription.cs
@@ -2,10 +2,7 @@
 {
     public class Prescription
     {
-        //public string AppointmentId { get; set; }
-        //public string Symptoms { get; set; }
-        //public string Diagnosis { get; set; }
-        //public List<PrescribedMedicine> Medicines { get; set; } = new();
+        
 
         public int PrescriptionId { get; set; }  // Optional for creation
         public int AppointmentId { get; set; }
